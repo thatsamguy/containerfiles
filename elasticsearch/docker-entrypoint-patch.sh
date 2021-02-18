c20f54661582aa3214b7ca9823d07c83126a6e8f
Sam Bailey <sam@thatsamguy.com>
2021-02-18 19:36:52 +1100

    add elasticsearch based on aws distro with mods

---
+++ elasticsearch/docker-entrypoint-patch.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+cp -f /usr/local/bin/docker-entrypoint.sh /usr/local/bin/docker-entrypoint.sh.orig && \
+head -n -1 /usr/local/bin/docker-entrypoint.sh.orig > /usr/local/bin/docker-entrypoint.sh && \
+{
+cat <<EOF
+# Also expand env vars in the format:
+#  ESOPT_cluster_name
+# to
+#  -Ecluster.name
+while IFS='=' read -r envvar_key envvar_value
+do
+    # Elasticsearch settings need to have at least two dot separated lowercase
+    # words, e.g. 'cluster.name', except for 'processors' which we handle
+    # specially
+    if [[ "\$envvar_key" =~ ^ESOPT_[a-z0-9_]+\_[a-z0-9_]+ || "\$envvar_key" == "ESOPT_processors" ]]; then
+        if [[ ! -z \$envvar_value ]]; then
+            envvar_real_key="$(echo "\${envvar_key}" | sed 's/\_/\./g' | cut -c7-256)"
+            es_opt="-E\${envvar_real_key}=\${envvar_value}"
+            es_opts+=("\${es_opt}")
+        fi
+    fi
+done < <(env)
+
+EOF
+} >> /usr/local/bin/docker-entrypoint.sh && \
+tail -n 1 /usr/local/bin/docker-entrypoint.sh.orig >> /usr/local/bin/docker-entrypoint.sh && \
+rm -f /usr/local/bin/docker-entrypoint.sh.orig
